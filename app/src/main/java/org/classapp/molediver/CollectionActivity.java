9073532baa25ff8c9abe75de3569f6a73e4d518a
RiccardoMarioBonato <Riccardomario.b@ku.th>
2026-03-10 17:52:23 +0700

    feat: added navigation system

---
+++ app/src/main/java/org/classapp/molediver/CollectionActivity.java
@@ -0,0 +1,17 @@
+package org.classapp.molediver;
+
+import android.os.Bundle;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CollectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_collection);
+
+        Button btnBack = findViewById(R.id.btnBack);
+        btnBack.setOnClickListener(v -> finish());
+    }
+}
---
+++ app/src/main/java/org/classapp/molediver/GameActivity.java
@@ -0,0 +1,12 @@
+package org.classapp.molediver;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GameActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // setContentView(R.layout.activity_game); // Uncomment when layout is created
+    }
+}
